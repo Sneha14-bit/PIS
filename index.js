66bf3d8a11fa95d94f1bf844687c4535522d823b
TheAmritpal <amritpal@hotmail.co.in>
2023-10-25 20:22:03 +0530

    changed in index file

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 //entry point
-const express = require('express');
-const db = require('./db/db');
-const router = require('./route/route');
+const express = require("express");
+const db = require("./db/db");
+const router = require("./route/route");
 const app = express();
-app.use(express.json())
-app.use(router)
+app.use(express.json());
+app.use(router);
 
 //app.post - to send data
 //app.get - to fetch data
 //const PORT = 5000;
-app.listen(5000, () => console.log('Server started'));
+
+app.listen(5000, () => console.log("Server started"));
